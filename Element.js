3a26d9f977f211e397a59e9f1e0205efa155bd8f
AbeilleTueuse <baptiste.ferrato1@orange.fr>
2024-03-07 14:12:08 +0100

    formatted

--- Element.js
+++ Element.js
@@ -1,7 +1,6 @@
 function camelToKebab(str) {
-  return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+  return str.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
 }
-
 
 function copyProperties(source, target, allowedProps) {
   var sourceKeys = Object.keys(source);
@@ -10,43 +9,61 @@ function copyProperties(source, target, allowedProps) {
     var property = sourceKeys[keyIndex];
     var propValue = source[property];
 
-    if (property === 'viewBox') {
+    if (property === "viewBox") {
       target.setAttribute(property, propValue);
-
-    } else if (property.indexOf("data") === 0 || allowedProps.indexOf(property) !== -1) {
+    } else if (
+      property.indexOf("data") === 0 ||
+      allowedProps.indexOf(property) !== -1
+    ) {
       target.setAttribute(camelToKebab(property), propValue);
     }
   }
 }
 
-
-function createElement(elementsToReplace, elementsNS, allowedProperties, elementDataset, elementType) {
-  
+function createElement(
+  elementsToReplace,
+  elementsNS,
+  allowedProperties,
+  elementDataset,
+  elementType
+) {
   if (elementsNS.indexOf(elementType) !== -1) {
-    var newElement = document.createElementNS("http://www.w3.org/2000/svg", elementType);
+    var newElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      elementType
+    );
   } else {
     var newElement = document.createElement(elementType);
   }
-  
+
   var childdNodes = elementsToReplace.childNodes;
-      
+
   copyProperties(elementDataset, newElement, allowedProperties);
-      
+
   for (var childIndex = 0; childIndex < childdNodes.length; childIndex++) {
     newElement.appendChild(childdNodes[childIndex].cloneNode(true));
   }
-  
+
   return newElement;
 }
 
-
-function replaceElement(elementsToReplace, allowedElements, elementsNS, allowedProperties) {
-  
+function replaceElement(
+  elementsToReplace,
+  allowedElements,
+  elementsNS,
+  allowedProperties
+) {
   var elementDataset = elementsToReplace.dataset;
   var elementType = elementDataset.element;
-  
+
   if (allowedElements.indexOf(elementType) !== -1) {
-    var newElement = createElement(elementsToReplace, elementsNS, allowedProperties, elementDataset, elementType);
+    var newElement = createElement(
+      elementsToReplace,
+      elementsNS,
+      allowedProperties,
+      elementDataset,
+      elementType
+    );
 
     elementsToReplace.parentNode.replaceChild(newElement, elementsToReplace);
   } else {
@@ -54,13 +71,12 @@ function replaceElement(elementsToReplace, allowedElements, elementsNS, allowedP
   }
 }
 
-
 function addCollapsible(toggleButton) {
   toggleButton.setAttribute("tabindex", 0);
   var collapsibleContent = toggleButton.nextElementSibling.nextElementSibling;
   var expandedClass = "mw-collapsible-toggle-expanded";
   var isCollapsed = false;
-  toggleButton.addEventListener("click", function(event) {
+  toggleButton.addEventListener("click", function (event) {
     if (isCollapsed) {
       this.classList.remove(expandedClass);
       collapsibleContent.style.maxHeight = null;
@@ -68,17 +84,18 @@ function addCollapsible(toggleButton) {
     } else {
       this.classList.add(expandedClass);
       collapsibleContent.classList.remove("tabber-noactive");
-      collapsibleContent.style.maxHeight = collapsibleContent.scrollHeight + 100 + "px";
+      collapsibleContent.style.maxHeight =
+        collapsibleContent.scrollHeight + 100 + "px";
     }
     isCollapsed = !isCollapsed;
   });
-  toggleButton.addEventListener("keyup", function(event) {
+  toggleButton.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
       this.click();
     }
   });
-  collapsibleContent.addEventListener("transitionend", function(event) {
-    console.log(isCollapsed)
+  collapsibleContent.addEventListener("transitionend", function (event) {
+    console.log(isCollapsed);
     if (isCollapsed) {
       this.style.overflow = "visible";
     } else {
@@ -87,22 +104,64 @@ function addCollapsible(toggleButton) {
   });
 }
 
-
-(function(){
-  
-  var allowedElements = ["input", "button", "select", "option", "label", "form", "svg", "path", "title"];
+(function () {
+  var allowedElements = [
+    "input",
+    "button",
+    "select",
+    "option",
+    "label",
+    "form",
+    "svg",
+    "path",
+    "title",
+  ];
   var elementsNS = ["svg", "path", "title"];
-  var allowedProperties = ["id", "class", "type", "name", "ariaHaspopup", "ariaExpanded", "ariaControls", "for", "min", "max", "value", "style", "pattern", "required", "selected", "disabled", "checked", "tabindex", "placeholder", "xmlns", "height", "viewBox", "width", "fill", "d", "title"];
-  
+  var allowedProperties = [
+    "id",
+    "class",
+    "type",
+    "name",
+    "ariaHaspopup",
+    "ariaExpanded",
+    "ariaControls",
+    "for",
+    "min",
+    "max",
+    "value",
+    "style",
+    "pattern",
+    "required",
+    "selected",
+    "disabled",
+    "checked",
+    "tabindex",
+    "placeholder",
+    "xmlns",
+    "height",
+    "viewBox",
+    "width",
+    "fill",
+    "d",
+    "title",
+  ];
+
   var elementsToReplace = document.querySelector("div[data-element]");
-  
+
   while (elementsToReplace) {
-    replaceElement(elementsToReplace, allowedElements, elementsNS, allowedProperties);
+    replaceElement(
+      elementsToReplace,
+      allowedElements,
+      elementsNS,
+      allowedProperties
+    );
     elementsToReplace = document.querySelector("div[data-element]");
   }
 
-  var toggleButtons = document.querySelectorAll(".improved-collapsible.custom-js > .mw-collapsible-toggle");
-  
+  var toggleButtons = document.querySelectorAll(
+    ".improved-collapsible.custom-js > .mw-collapsible-toggle"
+  );
+
   for (var toggleButton of toggleButtons) {
     addCollapsible(toggleButton);
   }
