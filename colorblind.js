914e168bbd6389e381b7ca3db63d22bd0d46457d
AbeilleTueuse <baptiste.ferrato1@orange.fr>
2025-05-25 13:30:30 +0200

    exclude safari for copy image

--- colorblind.js
+++ colorblind.js
@@ -31,7 +31,8 @@ class SpawnLayerManager {
   }
 
   _isClipboardImageSupported() {
-    return !!(navigator.clipboard?.write && window.ClipboardItem);
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    return !!(navigator.clipboard?.write && window.ClipboardItem) && !isSafari;
   }
 
   _initialize() {
